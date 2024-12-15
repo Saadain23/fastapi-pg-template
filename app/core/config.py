2b80b65928af36b79feb6cba3741d64f1ce39e38
Saadain23 <saadaink23@gmail.com>
2024-12-15 05:06:16 +0200

    initial setup

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,10 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Lasku Pay"
+    PROJECT_NAME: str = "GenMail"
     DATABASE_URL: str = str(os.getenv("DATABASE_URL"))
     SECRET_KEY: str = str(os.getenv("SECRET_KEY"))
-    ALGORITHM: str = "HS256"
+    JWT_ALGO: str = str(os.getenv("ALGORITHM"))
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.api import api_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
+    expose_headers=["*"],
+    max_age=600,
+)
 
 app.include_router(api_router, prefix="/api/v1")
